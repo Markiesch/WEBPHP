a373f5e3fd9601068961b28697120187667bb4a4
KorayYLM <pertev.koray@gmail.com>
2025-03-10 17:15:03 +0100

    API key werkt

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,19 @@
 import './bootstrap';
 import '../css/app.css';
+
+fetch('http://127.0.0.1:8000/api/register', {
+    method: 'POST',
+    headers: {
+        'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: 'test',
+        email: 'test2@gmail.com',
+        password: 'test1234',
+        password_confirmation: 'test1234'
+    })
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,14 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('api')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
+    Route::get('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
     Route::middleware('auth:sanctum')->group(function () {
+        // Protected routes
     });
 
     Route::get('/', function() {
         return 'API';
     });
-});
+
