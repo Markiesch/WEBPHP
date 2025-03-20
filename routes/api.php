f39319054724047cb3cde7636d7a7520cb29896e
KorayYLM <pertev.koray@gmail.com>
2025-03-20 15:44:46 +0100

    Api en login mixt up

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 ﻿<?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\ContractController;
 
-    Route::get('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-    Route::middleware('auth:sanctum')->group(function () {
-        // Protected routes
-    });
+Route::middleware('auth:sanctum')->group(function () {
+    // Protected routes
+});
 
 Route::post('export-pdf', [ContractController::class, 'exportPdf']);
 
-    Route::get('/', function() {
-        return 'API';
-    });
-
+Route::get('/', function() {
+    return 'API';
+});
