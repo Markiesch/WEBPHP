b3f7450322716fa4c78156a4a96b5cc644db2a1a
Markiesch <marksmeel@gmail.com>
2025-03-23 23:59:59 +0100

    Chore: cleanup

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,26 +3,3 @@ import './bootstrap';
 import UIkit from "franken-ui/js/core.iife";
 
 window.UIkit = UIkit;
-
-
-// const htmlElement = document.documentElement;
-//
-// const __FRANKEN__ = JSON.parse(
-//     localStorage.getItem("__FRANKEN__") || "{}",
-// );
-//
-// if (
-//     __FRANKEN__.mode === "dark" ||
-//     (!__FRANKEN__.mode &&
-//         window.matchMedia("(prefers-color-scheme: dark)").matches)
-// ) {
-//     htmlElement.classList.add("dark");
-// } else {
-//     htmlElement.classList.remove("dark");
-// }
-//
-// htmlElement.classList.add(__FRANKEN__.theme || "uk-theme-emerald");
-// htmlElement.classList.add(__FRANKEN__.radii || "uk-radii-md");
-// htmlElement.classList.add(__FRANKEN__.shadows || "uk-shadows-sm");
-// htmlElement.classList.add(__FRANKEN__.font || "uk-font-sm");
-// htmlElement.classList.add(__FRANKEN__.chart || "uk-chart-default");
