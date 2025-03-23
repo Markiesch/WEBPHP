40e4876fcfe24c2244ee5bad819fc3ebc00f8666
Markiesch <marksmeel@gmail.com>
2025-03-23 16:07:31 +0100

    Feat: models toegevoegd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Create roles
+        $roles = ['Admin', 'Editor', 'Viewer'];
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+
+        // Create users and assign roles
+        User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'role_id' => Role::where('name', 'Admin')->first()->id,
         ]);
     }
 }
