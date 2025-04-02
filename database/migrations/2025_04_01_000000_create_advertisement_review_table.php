48e88bdcc98a5e1887a61a41b70523ce97200c3e
Markiesch <marksmeel@gmail.com>
2025-04-02 20:51:23 +0200

    Unique constraint verwijderd

--- database/migrations/2025_04_01_000000_create_advertisement_review_table.php
+++ database/migrations/2025_04_01_000000_create_advertisement_review_table.php
@@ -15,9 +15,6 @@ return new class extends Migration
             $table->integer('rating')->comment('Rating from 1-5');
             $table->text('comment')->nullable();
             $table->timestamps();
-
-            // Ensure a user can only review an advertisement once
-            $table->unique(['user_id', 'advertisement_id']);
         });
     }
 
