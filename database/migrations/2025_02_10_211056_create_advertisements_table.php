45fed40ce688cbe0b47674fb4989c01497cd237e
KorayYLM <pertev.koray@gmail.com>
2025-04-03 16:29:23 +0200

    push for error

--- database/migrations/2025_02_10_211056_create_advertisements_table.php
+++ database/migrations/2025_02_10_211056_create_advertisements_table.php
@@ -10,6 +10,7 @@ return new class extends Migration
     {
         Schema::create('advertisements', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('business_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('description');
--- database/seeders/AdvertisementSeeder.php
+++ database/seeders/AdvertisementSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Advertisement;
-use Carbon\Carbon;
+use App\Models\Business;
 use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +14,9 @@ class AdvertisementSeeder extends Seeder
         $faker = Faker::create();
         $userId = 3; // business_advertiser ID
 
-        // Create 3 sale advertisements
+        $business = Business::where('user_id', $userId)->first();
+
+        // Create 4 sale advertisements
         for ($i = 0; $i < 4; $i++) {
             Advertisement::create([
                 'title' => $faker->words(3, true),
@@ -22,6 +24,7 @@ class AdvertisementSeeder extends Seeder
                 'price' => $faker->randomFloat(2, 200, 2000),
                 'wear_percentage' => $faker->numberBetween(0, 100),
                 'user_id' => $userId,
+                'business_id' => $business->id,
                 'type' => Advertisement::TYPE_SALE,
                 'image_url' => 'https://picsum.photos/800/600',
                 'expiry_date' => now()->addMonths(3),
@@ -36,6 +39,7 @@ class AdvertisementSeeder extends Seeder
                 'price' => $faker->randomFloat(2, 50, 500),
                 'wear_percentage' => $faker->numberBetween(0, 100),
                 'user_id' => $userId,
+                'business_id' => $business->id,
                 'type' => Advertisement::TYPE_RENTAL,
                 'image_url' => 'https://picsum.photos/800/600',
                 'rental_start_date' => now()->addDays($faker->numberBetween(1, 30)),
